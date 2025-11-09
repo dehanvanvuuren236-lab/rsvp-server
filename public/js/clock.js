67ac37286a53ba746e5d6469271cb63151a169f7
Dehan <dehanvanvuuren236@gmail.com>
2025-11-09 12:44:10 +0200

    updates

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,28 +1,23 @@
 $(document).ready(function () {
+  const currentDate = moment();
   const targetDate = moment.tz("2026-05-16 15:00", "Africa/Johannesburg");
-  const now = moment();
-  let diff = targetDate.unix() - now.unix();
+
+  let diff = targetDate.unix() - currentDate.unix();
   if (diff < 0) diff = 0;
 
-  const clock = $(".clock").FlipClock(diff, {
-    clockFace: "DailyCounter",
+  const clock = $('.clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
     countdown: true,
     autoStart: true,
     callbacks: {
-      start: function () {
-        console.log("⏳ Countdown started!");
-      },
       stop: function () {
-        console.log("✅ Countdown finished!");
+        console.log("⏰ Countdown finished!");
       }
     }
   });
 
-  const timerCheck = setInterval(() => {
-    if (clock.getTime().time <= 0) {
-      clock.setTime(0);
-      clock.stop();
-      clearInterval(timerCheck);
-    }
-  }, 1000);
+  // Force only one <li> per digit
+  $('.flip-clock-wrapper ul li').each(function () {
+    $(this).siblings().remove(); // remove duplicates
+  });
 });
