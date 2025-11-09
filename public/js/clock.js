e480be2351c0eab47a0a2261fcf2324d398c8efb
Dehan <dehanvanvuuren236@gmail.com>
2025-11-09 12:55:30 +0200

    updates

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,42 +1,25 @@
-$(document).ready(function () {
-  // Current date/time
-  const currentDate = moment();
-
-  // Target date in South Africa timezone
-  const targetDate = moment.tz("2026-05-16 15:00", "Africa/Johannesburg");
-
-  // Time difference in seconds
-  const diff = targetDate.unix() - currentDate.unix();
-
-  let clock;
-
-  if (diff <= 0) {
-    // If date already passed
-    clock = $(".clock").FlipClock(0, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autoStart: false
-    });
-    console.log("Date has already passed!");
-  } else {
-    // Initialize countdown
-    clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autoStart: true,
-      callbacks: {
-        stop: function () {
-          console.log("⏰ Countdown finished!");
-        }
-      }
-    });
-
-    // Prevent negative numbers
-    setInterval(() => {
-      if (clock.getTime() <= 0) {
-        clock.setTime(0);
-        clock.stop();
-      }
-    }, 1000);
+var CustomCounter = FlipClock.Face.extend({
+  build: function() {
+    this.createList(this.factory.getTime().time);
+  },
+  createList: function(time) {
+    this.div = $('<ul class="flip-clock-wrapper single-number"><li><a><div class="up"><div class="inn">' + time + '</div></div><div class="down"><div class="out">' + time + '</div></div></a></li></ul>');
+    this.div.appendTo(this.factory.$el);
+  },
+  update: function() {
+    var time = this.factory.getTime().time;
+    this.div.find('.inn').text(time);
+    this.div.find('.out').text(time);
   }
+});
+
+$(document).ready(function () {
+  const targetDate = moment.tz("2026-05-16 15:00", "Africa/Johannesburg");
+  const diff = targetDate.unix() - moment().unix();
+
+  var clock = $('.clock').FlipClock(diff, {
+    clockFace: CustomCounter,
+    countdown: true,
+    autoStart: true
+  });
 });
