8bbb72937f446c6a85871e292771a0c3a6588b05
Dehan <dehanvanvuuren236@gmail.com>
2025-11-09 13:22:26 +0200

    updates

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,49 +1,17 @@
-$(document).ready(function() {
-  let clock;
+$(document).ready(function () {
+  const targetDate = moment.tz("2026-05-16 15:00", "Africa/Johannesburg");
+  const now = moment();
+  let diff = targetDate.unix() - now.unix();
+  if (diff < 0) diff = 0;
 
-  // Grab the current date
-  let currentDate = new Date();
-
-  // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-05-16 15:00", "South Africa/Johannesburg");
-
-  // Calculate the difference in seconds between the future and current date
-  let diff = targetDate / 1000 - currentDate.getTime() / 1000;
-
-  if (diff <= 0) {
-    // If remaining countdown is 0
-    clock = $(".clock").FlipClock(0, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autostart: false
-    });
-    console.log("Date has already passed!")
-    
-  } else {
-    // Run countdown timer
-    clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      callbacks: {
-        stop: function() {
-          console.log("Timer has ended!")
-        }
+  const clock = $('.clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
+    countdown: true,
+    autoStart: true,
+    callbacks: {
+      stop: function () {
+        console.log("⏰ Countdown finished!");
       }
-    });
-    
-    // Check when timer reaches 0, then stop at 0
-    setTimeout(function() {
-      checktime();
-    }, 1000);
-    
-    function checktime() {
-      t = clock.getTime();
-      if (t <= 0) {
-        clock.setTime(0);
-      }
-      setTimeout(function() {
-        checktime();
-      }, 1000);
     }
-  }
+  });
 });
